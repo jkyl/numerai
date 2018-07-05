bca219eb8c8b3ed2b21835c88b56e723cf11f7fb
Jonathan Kyl <jonathan5kyl@gmail.com>
2018-07-05 01:24:03 -0400

    grid_search function (repeated)

--- models.py
+++ models.py
@@ -13,6 +13,15 @@ from sklearn.model_selection import StratifiedKFold, GridSearchCV
 
 from xgboost.sklearn import XGBClassifier
 
+def grid_search(model, param_grid, n_splits):
+  return GridSearchCV(model, param_grid,
+     cv=StratifiedKFold(n_splits=n_splits),
+     scoring='neg_log_loss',
+     refit=True,
+     n_jobs=-1,
+     verbose=2,
+   )
+
 def linear(X, y, n_splits=3, **kwargs):
   model = Pipeline([
     ('pca', PCA(whiten=True)),
@@ -24,39 +33,30 @@ def linear(X, y, n_splits=3, **kwargs):
       tol=1e-3,
     ))
   ])
-  search = GridSearchCV(model,
-    param_grid={'pca__n_components': np.arange(1, 51)},
-    cv=StratifiedKFold(n_splits=n_splits),
-    scoring='neg_log_loss',
-    refit=True,
-    n_jobs=-1,
-    verbose=2,
-  ).fit(X, y)
-  print('n_components: '+str(search.best_params_['pca__n_components']))
-  print('C: '+str(search.best_estimator_.steps[1][1].C_[0]))
-  print('CV loss: '+str(-search.best_score_))
+  param_grid = {
+    'pca__n_components': np.arange(1, 51),
+  }
+  search = grid_search(model, param_grid, n_splits).fit(X, y)
+  best_params = search.best_params_
+  best_params['lr__C'] = search.best_estimator_.steps[1][1].C_[0]
+  print(best_params)
+  print(-search.best_score_)
   return search.best_estimator_
 
 def adaboost(X, y, n_splits=3, **kwargs):
   model = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=1, min_samples_leaf=1))
-  search = GridSearchCV(model,
-    param_grid={
-      'n_estimators': np.arange(2, 12),
-      'learning_rate': [0.1],
-    },
-    cv=StratifiedKFold(n_splits=n_splits),
-    scoring='neg_log_loss',
-    refit=True,
-    n_jobs=-1,
-    verbose=2,
-  ).fit(X, y)
+    DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+  )
+  param_grid={
+    'n_estimators': np.arange(2, 12),
+    'learning_rate': [0.1],
+  }
+  search = grid_search(model, param_grid, n_splits).fit(X, y)
   print(search.best_params_)
   print(-search.best_score_)
   return search.best_estimator_
 
 def xgboost(X, y, n_splits=3, **kwargs):
-
   model = XGBClassifier(
     max_depth=1,
     learning_rate=0.1,
@@ -64,20 +64,13 @@ def xgboost(X, y, n_splits=3, **kwargs):
     colsample_bytree=1,
     objective='binary:logistic'
   )
-  search = GridSearchCV(
-    model,
-    param_grid={
-      'n_estimators': [100, 120, 140],
-      'subsample': [0.4, 0.6, 0.8],
-      'reg_lambda': [0.01, 0.1, 1],
-      'gamma': [1, 2, 4],
-    },
-    cv=StratifiedKFold(n_splits=n_splits),
-    scoring='neg_log_loss',
-    refit=True,
-    n_jobs=-1,
-    verbose=2,
-    ).fit(X, y)
+  param_grid={
+    'n_estimators': [100, 120, 140],
+    'subsample': [0.4, 0.6, 0.8],
+    'reg_lambda': [0.01, 0.1, 1],
+    'gamma': [1, 2, 4],
+  }
+  search = grid_search(model, param_grid, n_splits).fit(X, y)
   print(search.best_params_)
   print(-search.best_score_)
   return search.best_estimator_
